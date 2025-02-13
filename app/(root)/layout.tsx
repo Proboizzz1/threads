4263ff378d7a11d17c514d532999eb784c4becbe
Bilal <unknowx786@gmail.com>
2025-02-13 15:16:38 +0500

    first commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,8 +13,8 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads application",
+  title: "SnapGramX",
+  description: "Welocme To SnapGramX",
 };
 
 export default function RootLayout({
